b889531776db12e944f53967898df5e5c200fefd
MasterchiefCC <chenchenstudent@gmail.com>
2017-10-18 03:38:22 -0700

    homework2 draw

--- homework2/hw2.ml
+++ homework2/hw2.ml
@@ -1,3 +1,7 @@
+type ('terminal, 'nonterminal) symbol =
+  | T of 'terminal
+  | N of 'nonterminal
+
 let convert_grammar gram1 = match gram1 with
 (start, rules)-> let rec production nt prules = (match prules with 
                              |(stl, rule)::t->if stl = nt 
@@ -6,3 +10,36 @@ let convert_grammar gram1 = match gram1 with
                             |[]->[]
 ) in
 (start, (fun nt -> production nt rules))
+;;
+
+let rec match_list productionf list symbol=match list with
+|[]->false;;
+|(T tterm)::tail->if tterm = symbol
+                      then true 
+                      else match_list productionf tail symbol
+|(N nterm)::tail->match_table productionf (productionf nterm) symbol
+
+and
+
+rec match_table productionf rules symbol=match rules with
+|[]->false;;
+|head::tail->if match_list productionf head symbol
+                  then true 
+                  else match_table productionf tail symbol 
+;;
+
+
+
+
+
+let rec match_list 
+
+let rec matcher productionf rules dev frag =match rules with
+|head::tail->match head with 
+                    |(N nterm)->matcher productionf (productionf nterm)
+                    |(T tterm)->if List.hd(frag)=tterm
+                                       then true
+                                       else 
+
+let parse_prefix gram acceptor frag =
+  let matcher  (snd gram) ((snd gram) (fst gram))
--- homework2/hw2test.ml
+++ homework2/hw2test.ml
@@ -1,42 +1,50 @@
-type ('nonterminal, 'terminal) symbol =
-  | N of 'nonterminal
+type ('terminal, 'nonterminal) symbol =
   | T of 'terminal
+  | N of 'nonterminal
 
 type awksub_nonterminals =
-  | Expr | Lvalue | Incrop | Binop | Num
+  | Expr | Term | Lvalue | Incrop | Binop | Num
 
-let awksub_rules =
-   [Expr, [T"("; N Expr; T")"];
-    Expr, [N Num];
-    Expr, [N Expr; N Binop; N Expr];
-    Expr, [N Lvalue];
-    Expr, [N Incrop; N Lvalue];
-    Expr, [N Lvalue; N Incrop];
-    Lvalue, [T"$"; N Expr];
-    Incrop, [T"++"];
-    Incrop, [T"--"];
-    Binop, [T"+"];
-    Binop, [T"-"];
-    Num, [T"0"];
-    Num, [T"1"];
-    Num, [T"2"];
-    Num, [T"3"];
-    Num, [T"4"];
-    Num, [T"5"];
-    Num, [T"6"];
-    Num, [T"7"];
-    Num, [T"8"];
-    Num, [T"9"]]
+let grammar =
+    (Expr,
+     function
+       | Expr ->
+           [[N Term; N Binop; N Expr];
+            [N Term]]
+       | Term ->
+     [[N Num];
+      [N Lvalue];
+      [N Incrop; N Lvalue];
+      [N Lvalue; N Incrop];
+      [T"("; N Expr; T")"]]
+       | Lvalue ->
+     [[T"$"; N Expr]]
+       | Incrop ->
+     [[T"++"];
+      [T"--"]]
+       | Binop ->
+     [[T"+"];
+      [T"-"]]
+       | Num ->
+     [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
+      [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
 
-let convert_grammar gram1 = match gram1 with
-(start, rules)-> let rec production nt prules = (match prules with 
-                             |(stl, rule)::t->if stl = nt 
-                                                  then rule::(production nt t)
-                                                  else production nt t
-                            |[]->[]
-) in
-(start, (fun nt -> production nt rules))
+let (nt, pfun)=grammar
 
-let awksub_grammar = Expr, awksub_rules
-
-let awksub_grammar_2=convert_grammar awksub_grammar
+let rec match_list productionf list symbol=match list with
+  |[]->false
+  |(T tterm)::tail->if tterm = symbol
+                        then true 
+                        else match_list productionf tail symbol
+  |(N nterm)::tail->match_table productionf (productionf nterm) symbol
+  
+  and
+  
+match_table productionf rules symbol=match rules with
+  |[]->false
+  |head::tail->if match_list productionf head symbol
+                    then true 
+                    else match_table productionf tail symbol 
+;;
+match_table pfun (pfun nt) "++"                
+  
